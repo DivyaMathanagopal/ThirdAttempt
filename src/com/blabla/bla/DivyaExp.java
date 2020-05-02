d77124e3ddcfab96ae4a4043629f38b152de8429
unknown <divya.madvem@gmail.com>
2020-05-02 16:13:34 +0530

    first change

---
+++ src/com/blabla/bla/DivyaExp.java
@@ -0,0 +1,10 @@
+package com.blabla.bla;
+
+public class DivyaExp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Second success");
+	}
+
+}
---
+++ src/com/blabla/bla/SampleBla.java
@@ -0,0 +1,11 @@
+package com.blabla.bla;
+
+public class SampleBla {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Success");
+	}
+	
+}
